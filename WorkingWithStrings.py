d8c5a29f540a1f9c3de17cc6d3b9085cac56fb8b
texphos <texphos@gmail.com>
2021-01-15 09:10:05 -0500

    Create WorkingWithStrings.py

---
+++ WorkingWithStrings.py
@@ -0,0 +1,44 @@
+
+print("Giraffe Academy")
+
+print("Giraffe\nAcademy")
+
+print("Giraffe\"Academy")
+
+print("Giraffe\Academy")
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+phrase = "League of Legends"
+print(phrase + " is cool")
+
+phrase = "Giraffe Academy"
+print(phrase.lower())
+
+phrase = "Giraffe Academy"
+print(phrase.upper())
+
+phrase = "Giraffe Academy"
+print(phrase.isupper())
+
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+
+phrase = "Giraffe  Academy"
+print(len(phrase))
+
+phrase = "Giraffe Academy"
+print(phrase[3])
+
+phrase = "Giraffe Academy"
+print(phrase.index("G"))
+
+phrase = "Giraffe Academy"
+print(phrase.index("a"))
+
+phrase = "Giraffe Academy"
+print(phrase.index("Acad"))
+
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
